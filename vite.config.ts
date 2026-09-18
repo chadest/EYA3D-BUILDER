514befd78bffbc2cc8b2ea73650935a7d226a02e
Chades't Creative <135223618+chadest@users.noreply.github.com>
2026-09-19 01:16:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: 'eya3D',
+    base: '/eya3D',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
